ce8196972d4400c3e6be712ebdf4e84a9d3fc879
Aksel Erit <aerit@greeny.cs.tlu.ee>
2016-02-11 12:45:55 +0200

    nimi

--- sample/helloworld.php
+++ sample/helloworld.php
@@ -1,1 +1,10 @@
-hello world
+<?php
+
+	// Variable starts with $, string has to use jutumärke, lime ends with semicolon
+	$first_name = "Aksel";
+	$last_name = "Erit";
+	
+	// näitab koodi brauserile
+	echo $first_name." ".$last_name;
+
+?>
