664a64fe3d1196edc8ed7983ed938cf6ee6cb942
Aksel Erit <aerit@greeny.cs.tlu.ee>
2016-02-11 12:55:22 +0200

    added if statement

--- sample/helloworld.php
+++ sample/helloworld.php
@@ -7,4 +7,23 @@
 	// näitab koodi brauserile
 	echo $first_name." ".$last_name;
 
+	// > graeter than
+	// < smaller than
+	// >= greater than or equal
+	// <= ..
+	// == equal
+	// != no equal
+	
+	$age = 5;
+	
+	//if age is smaller than or equal to 6, then you
+	if($age <= 5){
+		echo " yes you can sleep at lunchtime";
+	}else{
+		echo " no, you are at school";
+	
+		
+	}
+	
+	
 ?>
